637a6c7bdb13d2eb1554b172a81fc5c05d98cb40
BRAILIN MANUEL REYES DOMINGUEZ <br2016-0979@uce.edu.do>
2021-05-20 23:43:28 -0400

    First Commit

---
+++ SendSMSProject/SendSMSProject/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace SendSMSProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSendSMS = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtPost = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(32, 61);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(325, 34);
+            this.txtPhoneNumber.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Phone Number";
+            // 
+            // btnSendSMS
+            // 
+            this.btnSendSMS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSendSMS.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSendSMS.Location = new System.Drawing.Point(32, 362);
+            this.btnSendSMS.Name = "btnSendSMS";
+            this.btnSendSMS.Size = new System.Drawing.Size(106, 52);
+            this.btnSendSMS.TabIndex = 2;
+            this.btnSendSMS.Text = "Send";
+            this.btnSendSMS.UseVisualStyleBackColor = true;
+            this.btnSendSMS.Click += new System.EventHandler(this.btnSendSMS_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMessage.Location = new System.Drawing.Point(32, 233);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(325, 100);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // txtPost
+            // 
+            this.txtPost.Location = new System.Drawing.Point(32, 153);
+            this.txtPost.Name = "txtPost";
+            this.txtPost.Size = new System.Drawing.Size(325, 34);
+            this.txtPost.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 28);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Post";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 438);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnSendSMS);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPost);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSendSMS;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtPost;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SendSMSProject/SendSMSProject/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendSMSProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendSMS()
+        {
+            try
+            {
+                SerialPort sp = new SerialPort(
+                txtPost.Text,      // port name
+                38400,       // baud rate
+                Parity.None, // parity
+                8,           // bits
+                StopBits.One // stop bits
+            );
+
+                sp.Handshake = Handshake.None;
+                sp.WriteTimeout = -1;
+
+                sp.Open();
+                sp.WriteLine("AT" + Environment.NewLine);
+                Thread.Sleep(100);
+                sp.WriteLine("AT+CMGF=1" + Environment.NewLine);
+                Thread.Sleep(100);
+                sp.WriteLine("AT+CMGS=\"" + txtPhoneNumber.Text + "\"" + Environment.NewLine);
+                Thread.Sleep(100);
+                sp.WriteLine(txtMessage.Text + Environment.NewLine);
+                Thread.Sleep(100);
+                sp.Write(new byte[] { 26 }, 0, 1);
+                Thread.Sleep(100);
+
+                var response = sp.ReadExisting();
+                if (response.Contains("ERROR"))
+                {
+                    MessageBox.Show("Send Failed", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                    MessageBox.Show("SMS Send", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                sp.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSendSMS_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+
+            SendSMS();
+
+            Cursor = Cursors.Default;
+        }
+    }
+}
